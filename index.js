c5ee40f3712b61312a5e90c9380058b5810daeda
Fiodar Sazanavets <33926695+fiodarsazanavets@users.noreply.github.com>
2023-03-03 19:44:16 +0800

    Refactor for exercise.

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const WindowsPathSeparatorStrategy = require('./windowsPathSeparatorStrategy.js'
 
 var context = new PathSeparatorContext();
 
-var strategy = process.platform == 'win32' ? 
-    new WindowsPathSeparatorStrategy() :
-    new UnixPathSeparatorStrategy();
-
-context.setStrategy(strategy);
+var pathSeparator = process.platform == 'win32' ? 
+    '\'' :
+    '/';
 
 console.log('The path separator is .' + context.getPathSeparator());
